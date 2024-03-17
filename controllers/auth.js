1407eb596340f0647145c004377881803856a9fa
Gurashish Singh Gill <gurashish22@Gurashishs-MacBook-Air.local>
2024-03-17 12:52:39 +0530

    backend setup

---
+++ controllers/auth.js
@@ -0,0 +1,5 @@
+/** @format */
+
+export const register = async () => {};
+
+export const login = async () => {};
---
+++ index.js
@@ -0,0 +1,37 @@
+/** @format */
+
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import helmet from "helmet";
+import authRoutes from "./routes/auth.js";
+
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose
+	.connect(process.env.MONGODB_URL)
+	.then(() => {
+		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+	})
+	.catch((error) => console.log(`${error} did not connect`));
